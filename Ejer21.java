9fd39e165c8b429861f6fac59a935899fbf64143
Veritosclavijo <veronica.clavijo1411@ucaldas.edu.co>
2023-11-07 23:17:49 -0500

    Ejercicios completos

---
+++ Ejer21.java
@@ -0,0 +1,34 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+
+public class Ejer21{
+    static PrintStream screen = System.out;
+    static Scanner keyboard = new Scanner (System.in);
+    public static void main (String[]args){
+  
+        int numentero=0;
+        String mensaje="";
+
+        screen.println("Ingrese un número entero");
+        numentero= keyboard.nextInt();
+
+        if (numentero==0) {
+            mensaje="El número ingresado es cero";
+        }
+        else if(numentero>0){
+            mensaje="El número ingresado es positivo";
+        }
+        else{
+            mensaje="El número ingresado es negativo";
+
+        }
+
+        screen.println(mensaje);
+
+            
+        }
+        
+        
+    }
+
